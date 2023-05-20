8836d02182d863ede1e95c77376e065235cb00d8
co16661666 <62578553+co16661666@users.noreply.github.com>
2023-05-19 22:24:43 -0400

    Update PlayCharacter.java

--- PlayCharacter.java
+++ PlayCharacter.java
@@ -19,7 +19,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
  
-public class Character
+public class PlayCharacter
 {
     private String name;
     private BufferedImage[] image;
