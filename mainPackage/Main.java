03bc65d4536873fc07d612234946b2e2bf0fbf0a
co16661666 <62578553+co16661666@users.noreply.github.com>
2023-05-20 09:05:25 -0400

    Update Main.java

--- mainPackage/Main.java
+++ mainPackage/Main.java
@@ -14,6 +14,7 @@ import javax.swing.JPanel;
  */
 public class Main extends JFrame
 {
+    private static final long serialVersionUID = 7148504528835036003L;
     BufferStrategy bs;
     
     public Main()
