da55df72760f28021ff7129474d038ed5260f283
co16661666 <62578553+co16661666@users.noreply.github.com>
2023-05-19 21:52:00 -0400

    Create Player.java
    initial commit

---
+++ Player.java
@@ -0,0 +1,34 @@
+public class Player extends Character
+{
+    public Player(String name, String imageURL, int health, int x, int y, int width, int height, int frameCount)
+    {
+        super(name, imageURL, health, x, y, width, height, frameCount);
+    }
+    
+    public void update(boolean[] moves)
+    {
+        if (moves[0])
+        {
+            //w
+            super.setY(super.getY() - 1);
+        }
+        
+        if (moves[1])
+        {
+            //a
+            super.setX(super.getX() - 1);
+        }
+        
+        if (moves[2])
+        {
+            //s
+            super.setY(super.getY() + 1);
+        }
+        
+        if (moves[3])
+        {
+            //d
+            super.setX(super.getX() + 1);
+        }
+    }
+}
