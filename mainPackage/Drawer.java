901a1c2a520c6ff1a98e18b9af6d7f6e582912f0
co16661666 <62578553+co16661666@users.noreply.github.com>
2023-05-21 09:32:28 -0400

    Update Drawer.java

--- mainPackage/Drawer.java
+++ mainPackage/Drawer.java
@@ -160,7 +160,7 @@ public class Drawer extends JPanel implements KeyListener
         Toolkit.getDefaultToolkit().sync();
         
         System.out.println(System.nanoTime());
-        if (Math.abs(System.nanoTime() - prevT)  < 16660.7)
+        if (Math.abs(System.nanoTime() - prevT)  < 166600000.7)
         {
             System.out.println("fisldkj");
             try
