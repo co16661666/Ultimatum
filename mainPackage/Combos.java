4c75f395ea46e9606e3f4a5ec5ff22a35d871d48
co16661666 <62578553+co16661666@users.noreply.github.com>
2023-05-22 22:40:31 -0400

    Create Combos.java

---
+++ mainPackage/Combos.java
@@ -0,0 +1,13 @@
+public class Combos
+{
+  private int frameCount;
+  private int displayImageRow;
+  private int displayImageCol;
+  
+  public Combos()
+  {
+    frameCount = 0;
+    displayImageRow = 0;
+    displayImageCol = 0;
+  }
+}
