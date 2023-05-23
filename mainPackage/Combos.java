5d15ed6b351ede1e53dfa6dd9443500bea106e27
co16661666 <62578553+co16661666@users.noreply.github.com>
2023-05-23 08:25:10 -0400

    creating arraylist of combos

--- mainPackage/Combos.java
+++ mainPackage/Combos.java
@@ -1,13 +1,31 @@
 public class Combos
 {
     private int frameCount;
-    private int displayImageRow;
-    private int displayImageCol;
+    private boolean[][] combo = new boolean[60][7];
+    private static ArrayList<Combos> comboList = new ArrayList<Combos>();
   
     public Combos()
     {
         frameCount = 0;
-        displayImageRow = 0;
-        displayImageCol = 0;
+        allCom.add(this);
+    }
+    
+    public static updateComboList(boolean[] curKeys)
+    {
+        for (boolean x : comboList)
+        {
+            x.update(curKeys);
+        }
+    }
+    
+    public void update(boolean[] curKeys)
+    {
+        for (int i = 1; i < 60; i++)
+        {
+            boolean[] temp = combo[i];
+            combo[i - 1] = temp;
+        }
+        
+        combo[combo.length - 1] = curKeys;
     }
 }
