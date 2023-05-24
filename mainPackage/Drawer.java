a3046c5b53fa7022b3a623dfbb0f3abb44002a47
co16661666 <62578553+co16661666@users.noreply.github.com>
2023-05-23 22:15:48 -0400

    slowed

--- mainPackage/Drawer.java
+++ mainPackage/Drawer.java
@@ -154,7 +154,7 @@ public class Drawer extends JPanel implements KeyListener
     {
         Toolkit.getDefaultToolkit().sync();
         
-        if (Math.abs(System.nanoTime() - prevT)  < 16670000)
+        if (Math.abs(System.nanoTime() - prevT)  < 16670000 * 100)
         {
             try
             {
