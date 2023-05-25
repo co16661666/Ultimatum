d59a100d3dc502389c3b1d8abb244f808e4eaecf
co16661666 <62578553+co16661666@users.noreply.github.com>
2023-05-24 21:52:06 -0400

    Update Main.java

--- mainPackage/Main.java
+++ mainPackage/Main.java
@@ -18,20 +18,23 @@ public class Main extends JFrame
     
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
+            //Creates a Drawer class that has a method that will constantly redraw the screen
             Drawer panel = new Drawer();
             
             //Character(String name, String imageURL, int health, int x, int y, int width, int height, int frameCountRow, int frameCountCol)
-            //Background object
+            
+            //creating object for background
             panel.addCharacter(new PlayCharacter("bg", "https://codehs.com/uploads/6f98f64dfec467225d723bae02fe6c0a", 10000000, 0, 0, 1280, 720, 1, 1));
-            //Character1
+            //creates the character that the user will play
             panel.addCharacter(new Player("character0", "https://codehs.com/uploads/14cba35834e0c4b2db1ab584437131ae", 100, 0, 0, 288, 600, 2, 6));
             
-            panel.setBackground(Color.BLACK.darker());
-            JFrame frame = new JFrame("Ultimatum");
-            frame.setSize(1280, 720);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.getContentPane().add(panel, BorderLayout.CENTER);
-            frame.setVisible(true);
+            //Setting up the JFrame to draw on
+            panel.setBackground(Color.BLACK.darker()); //Black background
+            JFrame frame = new JFrame("Ultimatum"); //Declaring/instantiating JFrame
+            frame.setSize(1280, 720); //Size of JFrame
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Closes the frame if the window is closed
+            frame.getContentPane().add(panel, BorderLayout.CENTER); //Adding a panel to draw content on
+            frame.setVisible(true); //Displays the Screen
         });
     }
 }
