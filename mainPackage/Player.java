64aae4439470f989367ebcd357b963e7c6135918
co16661666 <62578553+co16661666@users.noreply.github.com>
2023-05-26 21:32:09 -0400

    Update Player.java from Codehs

--- mainPackage/Player.java
+++ mainPackage/Player.java
@@ -1,12 +1,15 @@
 package mainPackage;
 
+import java.util.*;
+
 public class Player extends PlayCharacter
 {
     private double xVelocity;
     private double yVelocity;
     private int ground;
     private int direction;
-    
+    private int yBox;
+    private int xBox;
     private Combos combozo;
     
     public Player(String name, String imageURL, int health, int x, int y, int width, int height, int frameCountRow, int frameCountCol)
@@ -25,23 +28,24 @@ public class Player extends PlayCharacter
     
     public void update(boolean[] keyInputs)
     {
-        //combozo.update(keyInputs);
+        combozo.update(keyInputs);
         
-        for (int i = 0; i < 60; i++)
-        {
-            for (int j = 0; j < 7; j++)
-            {
-                System.out.print(combozo.getCombo()[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
+        // for (int i = 0; i < 60; i++)
+        // {
+        //     for (int j = 0; j < 7; j++)
+        //     {
+        //         System.out.print(combozo.getCombo()[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+        // System.out.println();
         
         if (super.getY() <= ground)
         {
             keyInputs[0] = false;
             
             yVelocity += 2;
+            
         }
         else
         {
@@ -57,14 +61,17 @@ public class Player extends PlayCharacter
             //u
             if (combozo.checkCombo(4, 54, 57) && !combozo.checkCombo(4, 56, 60) || !combozo.checkCombo(4, 54, 59))
             {
-                System.out.println("combo");
-                super.setImage(direction, 1);
+                if (combozo.checkCombo(4, 54, 57) && !combozo.checkCombo(4, 56, 60))
+                    System.out.println("combo");
+                if(direction == 0)
+                    super.setImage(direction, 1);
+                else
+                    super.setImage(direction,4);
             }
         }
         else
         {
             super.setImage(direction, 0);
-            keyInputs[4] = false;
         }
         
         if (keyInputs[0])
@@ -72,7 +79,10 @@ public class Player extends PlayCharacter
             //w
             if (super.getY() <= ground + 1)
             {
-                super.setImage(0,4);
+                if(direction == 0)
+                    super.setImage(0,4);
+                else
+                    super.setImage(1,1);
                 yVelocity -= 30;
                 keyInputs[0] = false;
             }
@@ -80,27 +90,45 @@ public class Player extends PlayCharacter
         if (keyInputs[1])
         {
             //a
-            if (!(xVelocity < -10) && super.getY() > ground)
+            if (/*super.getY() > ground && combozo.checkCombo(1, 50, 59)*/!combozo.checkCombo(1, 59, 60))
+            {
+                System.out.println("dash");
+                xVelocity -= 30;
+            }
+            else if (!(xVelocity < -10) && super.getY() > ground)
+            {
                 xVelocity -= 1;
+            }
+            direction = 0;
         }
         if (keyInputs[2])
         {
             //s
-            super.setImage(direction, 3);
+            if(direction == 0)
+                super.setImage(direction, 3);
+            else
+                super.setImage(direction, 2);
         }
         if (keyInputs[3])
         {
             //d
             if (!(xVelocity > 10) && super.getY() > ground)
                 xVelocity += 1;
+            direction = 1;
         }
         
         if (super.getY() <= ground)
-            super.setImage(direction,4);
-            
-        combozo.update(keyInputs);
+            if(direction == 0)
+                super.setImage(direction,4);
+            else
+                super.setImage(direction,1);
             
         super.setX(super.getX() + (int) xVelocity);
         super.setY(super.getY() + (int) yVelocity);
+    }
+    
+    public boolean[][] getCombo()
+    {
+        return combozo.getCombo();
     }
 }
