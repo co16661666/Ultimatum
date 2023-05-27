1a322c156f8c690ee95be557127d0f1ea9434d61
co16661666 <62578553+co16661666@users.noreply.github.com>
2023-05-26 21:31:41 -0400

    Update Bot.java from Codehs

--- mainPackage/Bot.java
+++ mainPackage/Bot.java
@@ -1,5 +1,7 @@
 package mainPackage;
 
+import java.util.*;
+
 public class Bot extends Player
 {
     public Bot(String name, String imageURL, int health, int x, int y, int width, int height, int frameCountRow, int frameCountCol)
@@ -7,10 +9,16 @@ public class Bot extends Player
         super(name, imageURL, health, x, y, width, height, frameCountRow, frameCountCol);
     }
     
-//     @Override
-//     public void update(Player other)
-//     {
-//         boolean[] moves = new boolean[10];
-//         super.update(keys)
-//     }
+    public void update(Player other)
+    {
+        boolean[] moves = new boolean[10];
+        
+        if (Math.abs(other.getX() - super.getX()) < 30)
+        {
+            moves[4] = true;
+        }
+        super.update(moves);
+        
+        //System.out.println(super.getX());
+    }
 }
