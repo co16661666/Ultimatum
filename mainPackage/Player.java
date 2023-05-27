91d44aa77288a4242781473efe700ddd216cfebd
co16661666 <62578553+co16661666@users.noreply.github.com>
2023-05-26 21:54:51 -0400

    debugging

--- mainPackage/Player.java
+++ mainPackage/Player.java
@@ -32,6 +32,8 @@ public class Player extends PlayCharacter
         
         for (int i = 0; i < 60; i++)
         {
+            System.out.println(i + " ");
+            
             for (int j = 0; j < 7; j++)
             {
                 System.out.print(combozo.getCombo()[i][j] + " ");
