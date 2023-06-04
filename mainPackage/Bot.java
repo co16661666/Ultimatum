050acd6547b8d906f076d70f069c7aca30ae4da3
co16661666 <62578553+co16661666@users.noreply.github.com>
2023-06-04 19:11:22 -0400

    Update Bot.java

--- mainPackage/Bot.java
+++ mainPackage/Bot.java
@@ -13,6 +13,11 @@ public class Bot extends Player
     {
         boolean[] moves = new boolean[10];
         
+        if (other.getX() - super.getX() > 0)
+            direction = 0;
+        else
+            direction = 4;
+        
         // if (Math.abs(other.getX() - super.getX()) < 30)
         // {
         //     moves[4] = true;
