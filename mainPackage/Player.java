cc6444172cc0da4687121d64e15f6383671a4d79
co16661666 <62578553+co16661666@users.noreply.github.com>
2023-06-08 22:50:50 -0400

    Update Player.java

--- mainPackage/Player.java
+++ mainPackage/Player.java
@@ -145,7 +145,7 @@ public class Player extends PlayCharacter
                     attackPointX = super.getX() + 260;
                     
                 attackPointY = 216;
-                damage = 5;
+                damage = 3;
             }
         }
         
