0e0d3dd5bce9da81cdd9c9f9add4f4b8ce4d74ba
co16661666 <62578553+co16661666@users.noreply.github.com>
2023-06-09 23:08:45 -0400

    cleanup

--- mainPackage/PlayCharacter.java
+++ mainPackage/PlayCharacter.java
@@ -86,7 +86,6 @@ public class PlayCharacter
         }
         
         this.health = health;
-        System.out.println(x);
         this.x = x;
         this.y = y;
         this.width = width;
