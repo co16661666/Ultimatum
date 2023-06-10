cdfa32c399ada0e13823806d0d9dfdf36d3345e4
co16661666 <62578553+co16661666@users.noreply.github.com>
2023-06-09 22:59:20 -0400

    Update Drawer.java

--- mainPackage/Drawer.java
+++ mainPackage/Drawer.java
@@ -310,15 +310,15 @@ public class Drawer extends JPanel implements KeyListener
         //If statements to determine if the game has ended
         if (sprity.get(1).getHealth() < 0 && sprity.get(2).getHealth() < 0)
         {
-            System.out.println("TIE");
+            System.out.println("TIE: THE REAL VICTORY IS THE FRIENDS WE MADE ALONG THE WAY");
         }
         else if (sprity.get(1).getHealth() < 0)
         {
-            System.out.println("YOU LOST EVERYTHING. SUCH IS THE NATURE OF AN ULTIMATUM.");
+            System.out.println("DEFEAT: YOU LOST EVERYTHING. SUCH IS THE NATURE OF AN ULTIMATUM.");
         }
         else if (sprity.get(2).getHealth() < 0)
         {
-            System.out.println("YOU GOT AWAY... THIS TIME");
+            System.out.println("VICTORY: YOU GOT AWAY... THIS TIME");
         }
         else
         {
