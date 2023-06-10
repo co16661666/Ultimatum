eb6f8984f63fa58aa70bc51b9cd999ec821b3b76
co16661666 <62578553+co16661666@users.noreply.github.com>
2023-06-09 22:50:38 -0400

    Update Main.java

--- mainPackage/Main.java
+++ mainPackage/Main.java
@@ -20,8 +20,13 @@ public class Main extends JFrame
     private static final long serialVersionUID = 7148504528835036003L;
     
     public static void main(String[] args) {
+        //Instantiates a Scanner object for taking key inputs
         Scanner scan = new Scanner(System.in);
+        
+        //Instructions for the controls
         System.out.println("Welcome!!\nControls: Press W to jump, S to crouch, and use A and D to move left and right.\nTo attack, use U to light punch, I to heavy punch, and J to kick. Remember that you must wait for your previous move to finish before your next move can be performed.\nThe bar in the lower left corner is your current health points, while the bar in the lower right is your opponent's health points. The first person to lose all their health points is loser! Good Luck! Press enter to begin.");
+        
+        //Waits until enter is pressed
         scan.nextLine();
         
         SwingUtilities.invokeLater(() -> {
